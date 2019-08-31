bb69b03600fefa951c864e8de8634253e7d44dcb
SymbulF <apddef11@hotmail.com>
2019-08-30 21:53:01 -0300

    Projeto_com_pubnub
    Inicializado desenvolvimento usando API Pubnub.
    
    Atualizar com producao.
    
    Ler LEIA

---
+++ models.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 07:28:17 2019
+
+@author: apdde / mb
+
+Funções de Subscribe e Publish
+"""
+
+
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory, PNOperationType
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from pubnub.exceptions import PubNubException
+
+
+pnconfig = PNConfiguration()
+
+pnconfig.subscribe_key = 'uma_chave'
+pnconfig.publish_key = 'outra_chave'
+pnconfig.ssl = False
+
+pubnub = PubNub(pnconfig)
+
+
+def chamada_publicacao(envelope, status):
+    #Confere se pedido foi completado
+    
+    if not status.is_error():
+        pass #Mensagem publicada
+    else:
+        pass
+        #mensagem falhou publicacao
+        #pedido falhou
+        #pode ser repetida com [status retry]
+        #tratar erros aqui
+        
+        
+class ChamadaDeAssinatura(SubscribeCallback):
+    
+    def presenca(self, pubnub, presence):
+        pass
+        #tratar de dados de presenca de dispositivo
+        
+    def message(self, pubnub, message):
+        pass #trata novas mensagens localizadas em message.message
+        
+    def sinal(self, pubnub, signal):
+        pass
+        #tratar sinais de entrada
+
+        
+    def status(self, pubnub, status):
+        
+        if status.operation == PNOperationType.PNSubscribeOperation \
+            or status.operation == PNOperationType.PNUnsubscribeOperation:
+            
+             if status.category == PNStatusCategory.PNConnectedCategory:
+                pass
+                # inscrissão ao canal de comunicacao ocorreu normalmente
+             elif status.category == PNStatusCategory.PNReconnectedCategory:
+                 pass
+                # Significa que ocorreu um erro de conexao mas que ja foi restabelecida
+             elif status.category == PNStatusCategory.PNDisconnectedCategory:
+                 pass
+                # Ocorre quando se desfaz a inscricao a um canal de comunicacao
+                # Dispositivo/interface para de receber mensagens
+             elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                 pass
+                # Geralmente, erro com a internet. Tende a tentar reconectar automaticamente
+             elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+                 pass
+                # conexao nao permitida
+             else:
+                 pass
+                # Erro com conexao de internet
+        elif status.operation == PNOperationType.PNSubscribeOperation:
+            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
+            # For more information on how to configure heartbeat notifications through the status
+            # PNObjectEventListener callback, consult http://www.pubnub.com/docs/python/api-reference-configuration#configuration
+            if status.is_error():
+                pass
+                # There was an error with the heartbeat operation, handle here
+            else:
+                pass
+                # Heartbeat operation was successful
+        else:
+            pass
+            # Encountered unknown status type
+        
+#Lidando com desconexoes
+class ChamadaTrataDisconexao(SubscribeCallback):
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            #internet caiu / perdida. Tratar e reconectar
+            pubnub.reconect()
+        elif status.category == PNStatusCategory.PNTimeoutCategory:
+            # timeout na tentativa de comunicacao. Tratar e reconectar
+            
+            pubnub.reconect()
+        else:
+            logger.debug(status)
+            
+    def presence(self, pubnub, presence):
+        pass
+  
+    def message(self, pubnub, message):
+        pass
+ 
+    def signal(self, pubnub, signal):
+        pass
+   
+
+
+
+
+#pubnub.subscribe().channels('um_canal').execute()   #coloca o objeto para ouvir em um dado canal do pubnub
+
+
+##adicionar / remover ouvintes
+#
+#ouvinte = ChamadaDeAssinatura()
+#
+#pubnub.add_listener(ouvinte)
+#
+##depois de um tempo
+#pubnub.remove_listener(ouvinte)
+
+##Adicionando ouvinte tratador de erros
+#ouvinte_erros = ChamadaTrataDisconexao()
+#pubnub.add_listener(ouvinte_erros)
