aba147abdc60f6821ef6cf032fa3b70c74a23f41
Matheos M. Bortolossi <33439468+matheosmb@users.noreply.github.com>
2019-09-02 12:05:55 -0300

    criação do menu
    menu básico

---
+++ menu.py
@@ -0,0 +1,16 @@
+opcao = 0;
+
+while opcao != 4:
+    print("SELECIONE UMA DAS OPÇÕES");
+    print("[1] - Opção 1\n[2] - Opção 2\n[3] - Opção 3\n[4] - SAIR\n");
+    opcao = int(input("Digite sua opção: "));
+
+    if opcao == 1:
+        #Faça algo
+        pass
+    elif opcao == 2:
+        #Faça algo
+        pass
+    elif opcao == 3:
+        #Faça algo
+        pass
